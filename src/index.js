1c28fed92dbd74ce42718231ec255d44e89ff6c7
RylkovAlex <32511612+RylkovAlex@users.noreply.github.com>
2019-10-15 12:59:57 +0300

    module1-task3

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app.jsx';
+
+const app = (
+  <App/>
+);
+
+ReactDOM.render(app, document.getElementById(`root`));
