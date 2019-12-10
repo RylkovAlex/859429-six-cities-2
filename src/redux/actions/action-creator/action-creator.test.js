8d9af70e8be47401a125c460a43141c98b78943b
RylkovAlex <32511612+RylkovAlex@users.noreply.github.com>
2019-12-10 16:41:54 +0300

    дописал тесты на reducer и action-creator

--- src/redux/actions/action-creator/action-creator.test.js
+++ src/redux/actions/action-creator/action-creator.test.js
@@ -1,5 +1,6 @@
 import ActionCreator from "./action-creator";
-import {CHANGE_CITY} from "../action-types";
+import {CHANGE_CITY, CHANGE_ACTIVE_CARD, LOAD_OFFERS_SUCCESS, LOAD_OFFERS_ERROR, LOGIN_SUCCESS, CLEAR_USER, SEND_REVIEW_START, SEND_REVIEW_SUCCESS, SEND_REVIEW_ERROR, POST_FAVORITE_SUCCESS, LOAD_REVIEWS_START, LOAD_REVIEWS_ERROR, FETCH_START, FETCH_SUCCESS, FETCH_ERROR, LOAD_REVIEWS_SUCCESS} from "../action-types";
+import {reviewsMock} from "../../../mocks/reviews";
 
 describe(`Action creators work correctly`, () => {
   it(`ActionCreator.changeCity() returns correct action`, () => {
@@ -8,4 +9,110 @@ describe(`Action creators work correctly`, () => {
       city: {},
     });
   });
+
+  it(`ActionCreator.changeActiveCard() returns correct action`, () => {
+    expect(ActionCreator.changeActiveCard(1)).toEqual({
+      type: CHANGE_ACTIVE_CARD,
+      cardId: 1,
+    });
+  });
+
+  it(`ActionCreator.loadOffersSuccess() returns correct action`, () => {
+    expect(ActionCreator.loadOffersSuccess([])).toEqual({
+      type: LOAD_OFFERS_SUCCESS,
+      offers: [],
+    });
+  });
+
+  it(`ActionCreator.loadOffersError() returns correct action`, () => {
+    expect(ActionCreator.loadOffersError()).toEqual({
+      type: LOAD_OFFERS_ERROR,
+    });
+  });
+
+  it(`ActionCreator.loginSuccess() returns correct action`, () => {
+    expect(ActionCreator.loginSuccess({})).toEqual({
+      type: LOGIN_SUCCESS,
+      user: {}
+    });
+  });
+
+  it(`ActionCreator.clearUser() returns correct action`, () => {
+    expect(ActionCreator.clearUser()).toEqual({
+      type: CLEAR_USER,
+    });
+  });
+
+  it(`ActionCreator.sendReviewStart() returns correct action`, () => {
+    expect(ActionCreator.sendReviewStart()).toEqual({
+      type: SEND_REVIEW_START,
+    });
+  });
+
+  it(`ActionCreator.sendReviewSuccess() returns correct action`, () => {
+    expect(ActionCreator.sendReviewSuccess()).toEqual({
+      type: SEND_REVIEW_SUCCESS,
+    });
+  });
+
+  it(`ActionCreator.sendReviewError() returns correct action`, () => {
+    expect(ActionCreator.sendReviewError()).toEqual({
+      type: SEND_REVIEW_ERROR,
+    });
+  });
+
+  it(`ActionCreator.postFavoriteSuccess() returns correct action`, () => {
+    expect(ActionCreator.postFavoriteSuccess(1, true)).toEqual({
+      type: POST_FAVORITE_SUCCESS,
+      payload: {id: 1, status: true}
+    });
+  });
+
+  it(`ActionCreator.loadReviewsStart() returns correct action`, () => {
+    expect(ActionCreator.loadReviewsStart()).toEqual({
+      type: LOAD_REVIEWS_START,
+    });
+  });
+
+  it(`ActionCreator.loadReviewsSuccess() returns correct action`, () => {
+    const sortedReviews = reviewsMock.sort((a, b) => new Date(b.date) - new Date(a.date));
+    expect(ActionCreator.loadReviewsSuccess(reviewsMock)).toEqual({
+      type: LOAD_REVIEWS_SUCCESS,
+      reviews: sortedReviews,
+    });
+  });
+
+  it(`ActionCreator.loadReviewsError() returns correct action`, () => {
+    expect(ActionCreator.loadReviewsError()).toEqual({
+      type: LOAD_REVIEWS_ERROR,
+    });
+  });
+
+  it(`ActionCreator.fetchStart() returns correct action`, () => {
+    expect(ActionCreator.fetchStart()).toEqual({
+      type: FETCH_START,
+    });
+  });
+
+  it(`ActionCreator.fetchSuccess() returns correct action`, () => {
+    expect(ActionCreator.fetchSuccess()).toEqual({
+      type: FETCH_SUCCESS,
+    });
+  });
+
+  it(`ActionCreator.fetchError() returns correct action`, () => {
+    const error = {
+      url: `url/`,
+      message: `message`,
+    };
+    expect(ActionCreator.fetchError(`url/`, error)).toEqual({
+      type: FETCH_ERROR,
+      payload: {
+        url: `url/`,
+        message: `message`,
+        error,
+      }
+    });
+  });
+
 });
--- src/redux/reducers/app-reducer.test.js
+++ src/redux/reducers/app-reducer.test.js
@@ -1,196 +1,129 @@
 import appReducer, {appInitialState} from "./app-reducer";
-import {CHANGE_CITY, LOAD_OFFERS_SUCCESS, FETCH_SUCCESS, FETCH_START} from "../actions/action-types";
+import {CHANGE_CITY, LOAD_OFFERS_SUCCESS, FETCH_SUCCESS, FETCH_START, LOAD_REVIEWS_SUCCESS, LOAD_REVIEWS_START, CHANGE_ACTIVE_CARD, LOAD_OFFERS_ERROR, LOGIN_SUCCESS, CLEAR_USER, SEND_REVIEW_START, SEND_REVIEW_SUCCESS, POST_FAVORITE_SUCCESS} from "../actions/action-types";
 import {createAPI} from "../../api/api";
 import MockAdapter from "axios-mock-adapter";
 import {Operation} from "../actions/action-creator/action-creator";
-
-const offer1 = {
-  city: {
-    name: `Cologne`,
-    location: {
-      latitude: 50.938903,
-      longitude: 6.966074,
-      zoom: 10
-    }
-  },
-  id: 5,
-  previewImage: `/img/apartment-01.jpg`,
-  images: [`/img/room.jpg`, `/img/apartment-01.jpg`, `/img/apartment-02.jpg`, `/img/apartment-03.jpg`, `/img/studio-01.jpg`, `/img/apartment-01.jpg`],
-  title: `Beautiful & luxurious studio at great location`,
-  isFavorite: false,
-  isPremium: true,
-  rating: 3.5,
-  type: `apartment`,
-  bedrooms: 3,
-  maxAdults: 4,
-  price: 10,
-  goods: [`Cable TV`, `Washing machine`, `Coffee machine`, `Dishwasher`],
-  host: {
-    id: 3,
-    isPro: true,
-    name: `Angelina4`,
-    avatarUrl: `/img/avatar-angelina.jpg`,
-  },
-  description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
-  location: {
-    latitude: 50.938903,
-    longitude: 6.966074,
-    zoom: 8
-  },
-};
-
-const offer2 = {
-  city: {
-    name: `Paris`,
-    location: {
-      latitude: 48.851353,
-      longitude: 2.341412,
-      zoom: 10
-    }
-  },
-  id: 6,
-  previewImage: `/img/apartment-01.jpg`,
-  images: [`/img/room.jpg`, `/img/apartment-01.jpg`, `/img/apartment-02.jpg`, `/img/apartment-03.jpg`, `/img/studio-01.jpg`, `/img/apartment-01.jpg`],
-  title: `Beautiful & luxurious studio at great location`,
-  isFavorite: false,
-  isPremium: true,
-  rating: 1.5,
-  type: `apartment`,
-  bedrooms: 3,
-  maxAdults: 4,
-  price: 100,
-  goods: [`Cable TV`, `Washing machine`, `Coffee machine`, `Dishwasher`],
-  host: {
-    id: 3,
-    isPro: true,
-    name: `Angelina4`,
-    avatarUrl: `/img/avatar-angelina.jpg`,
-  },
-  description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
-  location: {
-    latitude: 48.851353,
-    longitude: 2.341412,
-    zoom: 8
-  },
-};
-
-const apiResponse = [
-  {
-    id: 1,
-    city: {
-      name: `Amsterdam`,
-      location: {
-        latitude: 52.370216,
-        longitude: 4.895168,
-        zoom: 10
-      }
-    },
-    // eslint-disable-next-line camelcase
-    preview_image: `img/1.png`,
-    images: [`img/1.png`, `img/2.png`],
-    title: `Beautiful & luxurious studio at great location`,
-    // eslint-disable-next-line camelcase
-    is_favorite: false,
-    // eslint-disable-next-line camelcase
-    is_premium: false,
-    rating: 4.8,
-    type: `apartment`,
-    bedrooms: 3,
-    // eslint-disable-next-line camelcase
-    max_adults: 4,
-    price: 120,
-    goods: [`Heating`, `Kitchen`, `Cable TV`, `Washing machine`, `Coffee machine`, `Dishwasher`],
-    host: {
-      id: 3,
-      // eslint-disable-next-line camelcase
-      is_pro: true,
-      name: `Angelina`,
-      // eslint-disable-next-line camelcase
-      avatar_url: `img/1.png`
-    },
-    description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
-    location: {
-      latitude: 52.35514938496378,
-      longitude: 4.673877537499948,
-      zoom: 8
-    }
-  }
-];
-
-const apiOffers = [
-  {
-    id: 1,
-    city: {
-      name: `Amsterdam`,
-      location: {
-        latitude: 52.370216,
-        longitude: 4.895168,
-        zoom: 10
-      }
-    },
-    previewImage: `img/1.png`,
-    images: [`img/1.png`, `img/2.png`],
-    title: `Beautiful & luxurious studio at great location`,
-    isFavorite: false,
-    isPremium: false,
-    rating: 4.8,
-    type: `apartment`,
-    bedrooms: 3,
-    maxAdults: 4,
-    price: 120,
-    goods: [`Heating`, `Kitchen`, `Cable TV`, `Washing machine`, `Coffee machine`, `Dishwasher`],
-    host: {
-      id: 3,
-      isPro: true,
-      name: `Angelina`,
-      avatarUrl: `img/1.png`
-    },
-    description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
-    location: {
-      latitude: 52.35514938496378,
-      longitude: 4.673877537499948,
-      zoom: 8
-    }
-  }
-];
+import {offersMock} from "../../mocks/offers";
+import {reviewsMock} from "../../mocks/reviews";
 
 describe(`appReducer works correctly`, () => {
   it(`reducer without additional parameters should return initial state`, () => {
     expect(appReducer(undefined, {})).toEqual(appInitialState);
   });
+
   it(`appReducer should change current city by a given value`, () => {
-    expect(appReducer({
+    const prevState = Object.assign(appInitialState, {
       city: {},
-      allOffers: [],
-    }, {
+    });
+    const newState = Object.assign(prevState, {
+      city: {
+        name: `Paris`,
+      },
+    });
+
+    expect(appReducer(prevState, {
       type: CHANGE_CITY,
       city: {
         name: `Paris`,
       },
-    })).toEqual({
-      city: {
-        name: `Paris`,
-      },
-      allOffers: [],
-    });
+    })).toEqual(newState);
   });
 
-  it(`appReducer should correctly set allOffers after fetching them`, () => {
-    expect(appReducer({
-      allOffers: [],
-      isAppReady: false,
-      loadOffersError: false,
-    }, {
-      type: LOAD_OFFERS_SUCCESS,
-      offers: [offer1, offer2]
-    })).toEqual({
-      allOffers: [offer1, offer2],
-      isAppReady: true,
-      loadOffersError: false,
+  it(`appReducer should change activeCard by a given value`, () => {
+    const prevState = Object.assign(appInitialState, {
+      activeCard: 1,
     });
+    const newState = Object.assign(prevState, {
+      activeCard: 2,
+    });
+
+    expect(appReducer(prevState, {
+      type: CHANGE_ACTIVE_CARD,
+      cardId: 2,
+    })).toEqual(newState);
   });
 
   it(`Should make a correct API call to /hotels`, function () {
+    const apiOffersResponse = [
+      {
+        id: 1,
+        city: {
+          name: `Amsterdam`,
+          location: {
+            latitude: 52.370216,
+            longitude: 4.895168,
+            zoom: 10
+          }
+        },
+        // eslint-disable-next-line camelcase
+        preview_image: `img/1.png`,
+        images: [`img/1.png`, `img/2.png`],
+        title: `Beautiful & luxurious studio at great location`,
+        // eslint-disable-next-line camelcase
+        is_favorite: false,
+        // eslint-disable-next-line camelcase
+        is_premium: false,
+        rating: 4.8,
+        type: `apartment`,
+        bedrooms: 3,
+        // eslint-disable-next-line camelcase
+        max_adults: 4,
+        price: 120,
+        goods: [`Heating`, `Kitchen`, `Cable TV`, `Washing machine`, `Coffee machine`, `Dishwasher`],
+        host: {
+          id: 3,
+          // eslint-disable-next-line camelcase
+          is_pro: true,
+          name: `Angelina`,
+          // eslint-disable-next-line camelcase
+          avatar_url: `img/1.png`
+        },
+        description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+        location: {
+          latitude: 52.35514938496378,
+          longitude: 4.673877537499948,
+          zoom: 8
+        }
+      }
+    ];
+
+    const mockOffers = [
+      {
+        id: 1,
+        city: {
+          name: `Amsterdam`,
+          location: {
+            latitude: 52.370216,
+            longitude: 4.895168,
+            zoom: 10
+          }
+        },
+        previewImage: `img/1.png`,
+        images: [`img/1.png`, `img/2.png`],
+        title: `Beautiful & luxurious studio at great location`,
+        isFavorite: false,
+        isPremium: false,
+        rating: 4.8,
+        type: `apartment`,
+        bedrooms: 3,
+        maxAdults: 4,
+        price: 120,
+        goods: [`Heating`, `Kitchen`, `Cable TV`, `Washing machine`, `Coffee machine`, `Dishwasher`],
+        host: {
+          id: 3,
+          isPro: true,
+          name: `Angelina`,
+          avatarUrl: `img/1.png`
+        },
+        description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+        location: {
+          latitude: 52.35514938496378,
+          longitude: 4.673877537499948,
+          zoom: 8
+        }
+      }
+    ];
+
     const dispatch = jest.fn();
     const api = createAPI(dispatch);
     const apiMock = new MockAdapter(api);
@@ -198,7 +131,7 @@ describe(`appReducer works correctly`, () => {
 
     apiMock
       .onGet(`/hotels`)
-      .reply(200, apiResponse);
+      .reply(200, apiOffersResponse);
 
     return offersLoader(dispatch, jest.fn(), api)
       .then(() => {
@@ -211,7 +144,259 @@ describe(`appReducer works correctly`, () => {
         });
         expect(dispatch).toHaveBeenNthCalledWith(3, {
           type: LOAD_OFFERS_SUCCESS,
-          offers: apiOffers,
+          offers: mockOffers,
+        });
+      });
+  });
+
+  it(`appReducer should correctly set allOffers after fetching them`, () => {
+    const prevState = Object.assign(appInitialState, {
+      allOffers: [],
+      isAppReady: false,
+      loadOffersError: false,
+    });
+
+    const newState = Object.assign(prevState, {
+      allOffers: offersMock,
+      isAppReady: true,
+    });
+
+    expect(appReducer(prevState, {
+      type: LOAD_OFFERS_SUCCESS,
+      offers: offersMock
+    })).toEqual(newState);
+  });
+
+  it(`appReducer should correctly set offers loading error`, () => {
+    const prevState = Object.assign(appInitialState, {
+      loadOffersError: false,
+    });
+    const newState = Object.assign(prevState, {
+      loadOffersError: true,
+    });
+
+    expect(appReducer(prevState, {
+      type: LOAD_OFFERS_ERROR,
+    })).toEqual(newState);
+  });
+
+  it(`Should make a correct API call to /login`, function () {
+    const apiUserResponse = {
+      // eslint-disable-next-line camelcase
+      avatar_url: `/static/avatar/2.jpg`,
+      email: `user@gmail.com`,
+      id: 1,
+      // eslint-disable-next-line camelcase
+      is_pro: false,
+      name: `User`,
+    };
+
+    const dispatch = jest.fn();
+    const api = createAPI(dispatch);
+    const apiMock = new MockAdapter(api);
+    const sendAuthData = Operation.sendAuthData({
+      email: `user@gmail.com`,
+      password: `12345`
+    });
+
+    apiMock
+      .onPost(`/login`)
+      .reply(200, apiUserResponse);
+
+    return sendAuthData(dispatch, jest.fn(), api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(3);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: FETCH_START,
+        });
+        expect(dispatch).toHaveBeenNthCalledWith(2, {
+          type: FETCH_SUCCESS,
+        });
+        expect(dispatch).toHaveBeenNthCalledWith(3, {
+          type: LOGIN_SUCCESS,
+          user: apiUserResponse,
+        });
+      });
+  });
+
+  it(`appReducer should correctly set user's data`, () => {
+    const mockUser = {
+      avatarUrl: `/static/avatar/2.jpg`,
+      email: `user@gmail.com`,
+      id: 1,
+      isPro: false,
+      name: `User`,
+    };
+
+    const prevState = Object.assign(appInitialState, {
+      user: null,
+    });
+
+    const newState = Object.assign(prevState, {
+      user: mockUser,
+    });
+
+    expect(appReducer(prevState, {
+      type: LOGIN_SUCCESS,
+      user: mockUser
+    })).toEqual(newState);
+  });
+
+  it(`appReducer should correctly clear user's data`, () => {
+    const mockUser = {
+      avatarUrl: `/static/avatar/2.jpg`,
+      email: `user@gmail.com`,
+      id: 1,
+      isPro: false,
+      name: `User`,
+    };
+
+    const prevState = Object.assign(appInitialState, {
+      user: mockUser,
+    });
+
+    const newState = Object.assign(prevState, {
+      user: null,
+    });
+
+    expect(appReducer(prevState, {
+      type: CLEAR_USER,
+    })).toEqual(newState);
+  });
+
+  it(`Should make a correct API call to /comments`, function () {
+    const apiReviewsResponse = [
+      {
+        id: 1,
+        user: {
+          id: 4,
+          // eslint-disable-next-line camelcase
+          is_pro: false,
+          name: `Max`,
+          // eslint-disable-next-line camelcase
+          avatar_url: `img/1.png`
+        },
+        rating: 4,
+        comment: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+        date: `2019-05-08T14:13:56.569Z`
+      }
+    ];
+
+    const mockReviews = [
+      {
+        id: 1,
+        user: {
+          id: 4,
+          isPro: false,
+          name: `Max`,
+          avatarUrl: `img/1.png`
+        },
+        rating: 4,
+        comment: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+        date: `2019-05-08T14:13:56.569Z`
+      }
+    ];
+
+    const dispatch = jest.fn();
+    const api = createAPI(dispatch);
+    const apiMock = new MockAdapter(api);
+    const hotelId = 1;
+    const reviewsLoader = Operation.loadReviews(hotelId);
+
+    apiMock
+      .onGet(`/comments/${hotelId}`)
+      .reply(200, apiReviewsResponse);
+
+    return reviewsLoader(dispatch, jest.fn(), api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(4);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: LOAD_REVIEWS_START,
+        });
+        expect(dispatch).toHaveBeenNthCalledWith(2, {
+          type: FETCH_START,
+        });
+        expect(dispatch).toHaveBeenNthCalledWith(3, {
+          type: FETCH_SUCCESS,
+        });
+        expect(dispatch).toHaveBeenNthCalledWith(4, {
+          type: LOAD_REVIEWS_SUCCESS,
+          reviews: mockReviews,
+        });
+      });
+  });
+
+  it(`Should correctly set reviews data, after loading them`, () => {
+    const prevState = Object.assign(appInitialState, {
+      reviews: [],
+    });
+
+    const newState = Object.assign(prevState, {
+      reviews: reviewsMock,
+    });
+
+    expect(appReducer(prevState, {
+      type: LOAD_REVIEWS_SUCCESS,
+      reviews: reviewsMock,
+    })).toEqual(newState);
+  });
+
+  it(`Should make a correct API post request to /comments`, function () {
+    const mockReview = reviewsMock[0];
+    const dispatch = jest.fn();
+    const api = createAPI(dispatch);
+    const apiMock = new MockAdapter(api);
+    const hotelId = 1;
+    const sendReview = Operation.sendReview(mockReview, hotelId);
+
+    apiMock
+      .onPost(`/comments/${hotelId}`)
+      .reply(200);
+
+    return sendReview(dispatch, jest.fn(), api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(5);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: SEND_REVIEW_START,
+        });
+        expect(dispatch).toHaveBeenNthCalledWith(2, {
+          type: FETCH_START,
+        });
+        expect(dispatch).toHaveBeenNthCalledWith(3, {
+          type: FETCH_SUCCESS,
+        });
+        expect(dispatch).toHaveBeenNthCalledWith(4, {
+          type: SEND_REVIEW_SUCCESS,
+        });
+      });
+  });
+
+  it(`Should make a correct API post request to /favorites`, function () {
+    const dispatch = jest.fn();
+    const api = createAPI(dispatch);
+    const apiMock = new MockAdapter(api);
+    const hotelId = 1;
+    const postFavorite = Operation.postFavorite(hotelId, true);
+
+    apiMock
+      .onPost(`favorite/${hotelId}/1`)
+      .reply(200);
+
+    return postFavorite(dispatch, jest.fn(), api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(3);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: FETCH_START,
+        });
+        expect(dispatch).toHaveBeenNthCalledWith(2, {
+          type: FETCH_SUCCESS,
+        });
+        expect(dispatch).toHaveBeenNthCalledWith(3, {
+          type: POST_FAVORITE_SUCCESS,
+          payload: {
+            id: hotelId,
+            status: true,
+          }
         });
       });
   });
